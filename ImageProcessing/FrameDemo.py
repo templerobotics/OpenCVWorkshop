710d02816ed7d58de892445fdd30b228a6f6be2e
swilso421 <swilso421@gmail.com>
2017-10-13 10:42:33 -0400

    Added comments

--- ImageProcessing/FrameDemo.py
+++ ImageProcessing/FrameDemo.py
@@ -7,22 +7,29 @@ loadpath_subject = os.path.join('FrameResources', 'willy.jpg')
 loadpath_frame = os.path.join('FrameResources', 'frame.png')
 savepath = os.path.join('FrameResources', 'merged.png')
 
+#Load the frame image
 frame = cv2.imread(loadpath_frame)
 
+#Convert the frame to grayscale so that it has a single channel
 frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-cv2.imwrite('frame_gray.png', frame_gray)
-
+#In the grayscale frame, create a mask which is white wherever there was color in the frame image
 _, frame_mask = cv2.threshold(frame_gray, 0, 255, cv2.THRESH_BINARY)
 
+#Invert the mask so that the white area represents the empty part of the frame
 frame_mask_negative = cv2.bitwise_not(frame_mask)
 
+#Load the subject that we want to put in the frame
 subject = cv2.imread(loadpath_subject)
 
+#Scale the subject to the same size as the frame. Some warping will be visible
 subject_scaled = cv2.resize(subject, (frame.shape[1], frame.shape[0]))
 
+#Using the inverted mask, trim the scaled subject into the shape of the inside of the frame
 subject_cutout = cv2.bitwise_and(subject_scaled, subject_scaled, mask = frame_mask_negative)
 
+#Combine the frame and the cutout of the subject into a new, merged image
 merged = cv2.bitwise_or(frame, subject_cutout)
 
+#Save the merged image
 cv2.imwrite(savepath, merged)
